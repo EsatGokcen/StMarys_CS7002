5663a71600d7df540c41fa99e5a1fda64428498d
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-19 18:44:39 +0100

    added two new files to week2

---
+++ week2/factorial.py
@@ -0,0 +1,14 @@
+
+number = int(input("Please enter a number: "))
+
+product = 1
+
+for count in range(1, number + 1, 1):
+    product *= count
+
+print(f"The factorial of {number} is {product}.")
+        
+    
+
+
+     

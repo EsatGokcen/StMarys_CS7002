052bcb148b266e0136700dff6ff29efe9f8c7f8c
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-19 21:45:21 +0100

    added two methods to planet

--- week3/planet.py
+++ week3/planet.py
@@ -1,6 +1,12 @@
 
 class Planet():
 
-    def __init__(self, name, humans):
-        self.name = name
-        self.humans = humans
+    def __init__(self, name="", humans=[]):
+        self.__name = name
+        self.__humans = humans
+
+    def __repr__(self):
+        return f'planet(name={self.__name}, humans={self.__humans})'
+
+    def __str__(self):
+        return f'Planet is called {self.__name} and is home to {self.__humans}'

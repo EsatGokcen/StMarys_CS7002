8c5f412286055e7d8010cb3b2c170c3b03f1eb66
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-21 16:57:22 +0100

    created clothing_size.py and updated clothing.py

--- week3/clothing.py
+++ week3/clothing.py
@@ -1,10 +1,11 @@
+from clothing_size import ClothingSize
 
 class Clothing():
 
     def __init__(self, colour, material):
         self.__colour = colour
         self.__material = material
-        self.__size = Clothing.ClothingSize() 
+        self.__size = ClothingSize() 
 
     def __repr__(self):
         return f'Clothing(colour={self.__colour} , material={self.__material} , size={self.__size})'
@@ -12,5 +13,3 @@ class Clothing():
     def __str__(self):
         return f'Clothing colour: {self.__colour} , clothing material: {self.__material} , clothing size: {self.__size}'
     
-    def ClothingSize(self): #ASK PRINS
-        self.__size == 1

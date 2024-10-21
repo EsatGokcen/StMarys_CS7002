97db80d2d42aac04df0602755c29646a1664d0f5
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-21 13:19:38 +0100

    completed planet.py

--- week3/planet.py
+++ week3/planet.py
@@ -1,12 +1,34 @@
+from human import Human
 
 class Planet():
 
-    def __init__(self, name="", humans=[]):
+    def __init__(self, name=""):
         self.__name = name
-        self.__humans = humans
+        self.__humans = []
 
     def __repr__(self):
-        return f'planet(name={self.__name}, humans={self.__humans})'
+        return f'planet(name={self.__name}, population={self.population()})'
 
     def __str__(self):
-        return f'Planet is called {self.__name} and is home to {self.__humans}'
+        return f'Planet is called {self.__name} and has a population of {self.population()}'
+    
+    def add(self, human):
+        if isinstance(human, Human):
+            self.__humans.append(human)
+            return True
+        return False
+
+    def remove(self, human):
+        if human in self.__humans:
+            self.__humans.remove(human)
+            return True
+        return False
+
+    def has(self, human):
+        return human in self.__humans
+
+    def population(self):
+        return len(self.__humans)
+
+
+    

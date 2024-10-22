eff6bc90f1aa81bf75913dec11941ef1704df325
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-22 20:06:23 +0100

    created plant class

---
+++ week4/plant.py
@@ -0,0 +1,14 @@
+
+class Plant():
+
+    def __init__(self, name):
+        self.__name = name
+
+    def __repr__(self):
+        return f''
+    
+    def __str__(self):
+        return f''
+    
+    def absorb(self, amount):
+        pass

e066c4c681bbfc9ab70c225f2e77bd09ba3a8960
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-22 19:44:17 +0100

    imported week4 living_thing to planet

--- week3/planet.py
+++ week3/planet.py
@@ -1,10 +1,10 @@
-from human import Human
+from week4 import living_thing as lv
 
 class Planet():
 
     def __init__(self, name=""):
         self.__name = name
-        self.__humans = []
+        self.__living_things = []
 
     def __repr__(self):
         return f'planet(name={self.__name}, population={self.population()})'
@@ -12,23 +12,23 @@ class Planet():
     def __str__(self):
         return f'Planet is called {self.__name} and has a population of {self.population()}'
     
-    def add(self, human):
-        if isinstance(human, Human):
-            self.__humans.append(human)
+    def add(self, living_thing):
+        if isinstance(living_thing, lv.LivingThing):
+            self.__living_things.append(living_thing)
             return True
         return False
 
     def remove(self, human):
-        if human in self.__humans:
-            self.__humans.remove(human)
+        if human in self.__living_things:
+            self.__living_things.remove(human)
             return True
         return False
 
     def has(self, human):
-        return human in self.__humans
+        return human in self.__living_things
 
     def population(self):
-        return len(self.__humans)
+        return len(self.__living_things)
 
 
     

4956370d7a4a523c557a2fcbb07364cb8cd20c26
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-22 19:28:29 +0100

    created livingthing class

---
+++ week4/living_thing.py
@@ -0,0 +1,19 @@
+
+class LivingThing():
+
+    def __init__(self, name, energy, age):
+        self.__name = name
+        self.__energy = energy
+        self.__age = age
+
+    def __repr__(self):
+        return f''
+    
+    def __str__(self):
+        return f''
+    
+    def grow(self):
+        pass
+
+    def reproduce(self):
+        pass

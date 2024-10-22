763cec6b6abb801f395b98ad237374b9535c72ee
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-22 20:03:14 +0100

    created animal class

---
+++ week4/animal.py
@@ -0,0 +1,19 @@
+
+class Animal():
+
+    def __init__(self, name):
+        self.__name = name
+
+    def __repr__(self):
+        return f''
+    
+    def __str__(self):
+        return f''
+    
+    def move():
+        pass
+
+    def eat():
+        pass
+        
+        

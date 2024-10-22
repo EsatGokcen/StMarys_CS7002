f041c3dcfb13086424810f17ef286e0731af9ad0
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-22 23:16:00 +0100

    completed initial livingThing

--- week4/living_thing.py
+++ week4/living_thing.py
@@ -9,13 +9,31 @@ class LivingThing():
         self.__age = age
 
     def __repr__(self):
-        return f''
+        return f'LivingThing(name={self.__name} , age={self.__age} , energy={self.__energy})'
     
     def __str__(self):
-        return f''
+        return f'{self.__name} is {self.__age} years old and has {self.__energy} energy.'
     
     def grow(self):
-        pass
+        did_grow = False
+        grow_cost = 50
+
+        if self.__energy >= grow_cost:
+            self.__age +=1
+            self.__energy -= grow_cost
+            did_grow = True
+
+        return did_grow
+        
 
     def reproduce(self):
-        pass
+        energy_required = 20
+        did_repro = False
+
+        if self.__energy >= energy_required:
+            self.__energy - energy_required
+            did_repro = True
+        else: 
+            did_repro = False
+
+        return did_repro

0a6c836b87c021292d720f86d340ca94700b5e28
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-23 21:05:50 +0100

    initial SuperPlant and InvisibleSP classes

---
+++ week4/invisibility_super_power.py
@@ -0,0 +1,8 @@
+
+class InvisibilitySuperPower():
+
+    def invisible():
+        return f'Subclass is invisible...'
+
+    def visible():
+        return f'Subclass is visible...'
---
+++ week4/super_plant.py
@@ -0,0 +1,17 @@
+
+from plant import Plant
+from invisibility_super_power import InvisibilitySuperPower
+
+class SuperPlant(Plant):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __repr__(self):
+        return super().__repr__()
+    
+    def __str__(self):
+        return super().__str__()
+    
+    def 
+    

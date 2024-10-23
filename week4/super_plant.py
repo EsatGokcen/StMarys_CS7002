b0dc513c63063049f42bcabeef55a38788d59599
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-23 21:07:35 +0100

    modified SuperPlant

--- week4/super_plant.py
+++ week4/super_plant.py
@@ -1,8 +1,8 @@
 
 from plant import Plant
-from invisibility_super_power import InvisibilitySuperPower
+from invisibility_super_power import InvisibilitySuperPower #NO CLUE HOW THIS WORKS
 
-class SuperPlant(Plant):
+class SuperPlant(Plant, InvisibilitySuperPower): #NO CLUE HOW THIS WORKS
 
     def __init__(self, name):
         super().__init__(name)
@@ -13,5 +13,14 @@ class SuperPlant(Plant):
     def __str__(self):
         return super().__str__()
     
-    def 
+    def invisible(): #NO CLUE HOW THIS WORKS
+        pass
+
+    def visible(): #NO CLUE HOW THIS WORKS
+        pass
+        
+        
+
+    
+
     

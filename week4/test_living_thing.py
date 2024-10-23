59d8bc96b3a1e02f8eb7765b1b5c35ac6318d89b
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-23 20:38:39 +0100

    created test for living_thing

---
+++ week4/test_living_thing.py
@@ -0,0 +1,28 @@
+#NOT MODIFIED - CHANGE FOR SPECIFIC CLASS BEING TESTED
+
+import unittest
+from human import Human   # type: ignore
+
+class HumanTestCase(unittest.TestCase):
+
+    def test_eat(self):
+        #Energy is full and try to eat
+        human_esat = Human("Esat")
+        self.assertEqual(human_esat.eat(20), 20, "Excess should be 20.")
+
+        # energy is below 100 and eat more than required
+        human_esat = Human("Esat" , energy = 90)
+        self.assertEqual(human_esat.eat(20), 10, "Excess should be 20.")
+
+        #energy is below 100 and eat exactly what is required
+        human_esat = Human("Esat" , energy = 80)
+        self.assertEqual(human_esat.eat(20), 0, "Excess should be 0.")
+
+        #energy is below 100 and eat less than required
+        human_esat = Human("Esat" , energy = 70)
+        self.assertEqual(human_esat.eat(20), -10, "Excess should be -10.")
+
+    #add additional tests here
+
+if __name__ == '__main__':
+    unittest.main()

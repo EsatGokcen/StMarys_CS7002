33dad78b744fc410a3bebfa03cb63ffd3fc804f4
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-23 11:01:36 +0100

    finished fungi class

--- week4/fungi.py
+++ week4/fungi.py
@@ -1,3 +1,29 @@
 
 from living_thing import LivingThing
 
+class Fungi(LivingThing):
+
+    def __init__(self, name, energy, age):
+        super().__init__(name, energy, age)
+
+    def __repr__(self):
+        return super().__repr__()
+    
+    def __str__(self):
+        return super().__str__()
+    
+    def saprotrophic_nutrition(self, amount):
+
+        breakdown = amount * 2
+        self.__energy += breakdown
+
+        if self.__energy > Fungi.MAX_ENERGY:
+            excess_amount = self.__energy - Fungi.MAX_ENERGY
+        elif self.__energy < Fungi.MAX_ENERGY:
+            excess_amount = self.__energy - Fungi.MAX_ENERGY
+        else:
+            excess_amount = 0
+
+        return excess_amount
+
+

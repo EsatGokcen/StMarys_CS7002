d793a002ac4996f21c969130aa30b1444eaf2a1c
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-24 10:52:42 +0100

    initial library class

---
+++ week5/library.py
@@ -0,0 +1,34 @@
+
+class Library:
+
+    def __init__(self):
+        self.__books = []
+        self.__patrons = []
+        self.__transactions = []
+
+    def add_book(self, book):
+        self.__books.append(book)
+
+    def remove_book(self, book):
+        self.__books.remove(book)
+
+    def add_patrons(self, patron):
+        self.__patrons.append(patron)
+
+    def remove_patrons(self, patron):
+        self.__patrons.remove(patron)
+
+    def lend_book(self, book, patron):
+        if book in self.__books and patron in self.__patrons:
+            self.__transactions.append((book, patron))
+            self.__books.remove(book)
+            return True
+        return False #IS THIS A PROPER WAY TO MAKE AN ELSE STATEMENT
+    
+    def return_book(self, book):
+        for transaction in self.__transactions:
+            if transaction[0] == book:
+                self.__books.append(book)
+                self.__transactions.remove(transaction)
+                return True
+        return False

8ca65f519fd38216d875e3c2daabb02be33bf03f
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-27 19:18:05 +0000

    Patron class modified id, initial dunder

--- week5/solid/patron.py
+++ week5/solid/patron.py
@@ -1,7 +1,18 @@
 
 class Patron:
 
-    def __init__(self, name, id):
+    next_patron_id = 1000
+
+    def __init__(self, name):
         self.__name = name
-        self.__id = id
+        self.__id = Patron.next_patron_id
+        Patron.next_patron_id += 1
+
         self.__patrons = []
+
+    def __repr__(self):
+        return f'Patron(name={self.__name} , id={self.__id})'
+    
+    def __str__(self):
+        return f'Patron name: {self.__name} \nPatron id: {self.__id}'
+    

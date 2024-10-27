45cdfa4171a05131c7ae3734bd384d858b2de060
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-27 19:06:43 +0000

    transaction id

--- week5/solid/transaction.py
+++ week5/solid/transaction.py
@@ -1,6 +1,9 @@
 
 class Transaction:
 
+    next_transaction_id = 1000
+
     def __init__(self):
-        pass
+        self.__id = Transaction.next_transaction_id
+        Transaction.next_transaction_id += 1
     

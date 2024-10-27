2132f3c7d4591b30627a30e6bb69958049dc1045
Esat Gokcen <esatgokcen173@gmail.com>
2024-10-27 19:14:56 +0000

    dunder methods for transaction class

--- week5/solid/transaction.py
+++ week5/solid/transaction.py
@@ -6,4 +6,11 @@ class Transaction:
     def __init__(self):
         self.__id = Transaction.next_transaction_id
         Transaction.next_transaction_id += 1
+        self.__transactions = []
+
+    def __repr__(self):
+        return f'Transaction(id={self.__id})'
+    
+    def __str__(self):
+        return f'Transaction id: {self.__id}'
     

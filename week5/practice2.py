100139fb5625c419c3e3e33f5482fcd5322f64bb
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-08 20:22:15 +0000

    initial truck class

--- week5/practice2.py
+++ week5/practice2.py
@@ -16,5 +16,15 @@ class Car:
     
     class Truck:
 
-        def __init__(self):
-            pass
+        def __init__(self, brand, model, fuel_tank_capacity, fuel_consumption):
+            self.__brand = brand
+            self.__model = model
+            self.__fuel_tank_capacity = fuel_tank_capacity
+            self.__fuel_consumption = fuel_consumption
+
+    def calculate_fuel_efficiency(self):
+        return self.__fuel_tank_capacity / self.__fuel_consumption
+
+    def load_goods(self):
+        return 'Loading goods'
+        

f49b8a27a9ced653e6cfbdee5ad8943fe06f0665
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-08 20:25:19 +0000

    initial motorcycle class

--- week5/practice2.py
+++ week5/practice2.py
@@ -14,17 +14,32 @@ class Car:
     def honk(self):
         return "Beep! Beep!"
     
-    class Truck:
+class Truck:
 
-        def __init__(self, brand, model, fuel_tank_capacity, fuel_consumption):
-            self.__brand = brand
-            self.__model = model
-            self.__fuel_tank_capacity = fuel_tank_capacity
-            self.__fuel_consumption = fuel_consumption
+    def __init__(self, brand, model, fuel_tank_capacity, fuel_consumption):
+        self.__brand = brand
+        self.__model = model
+        self.__fuel_tank_capacity = fuel_tank_capacity
+        self.__fuel_consumption = fuel_consumption
 
     def calculate_fuel_efficiency(self):
         return self.__fuel_tank_capacity / self.__fuel_consumption
 
     def load_goods(self):
         return 'Loading goods'
-        
+
+class Motorcylce:
+
+    def __init__(self, brand, model, fuel_tank_capacity, fuel_consumption):
+        self.__brand = brand
+        self.__model = model
+        self.__fuel_tank_capacity = fuel_tank_capacity
+        self.__fuel_consumption = fuel_consumption
+
+    def calculate_fuel_efficiency(self):
+        return self.__fuel_tank_capacity / self.__fuel_consumption
+
+    def wheelie(self):
+        return 'Performing a wheelie!'
+
+

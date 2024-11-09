c3f8c9ecc367d38dcf4bd84fede5d71f0cea7528
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-09 18:58:11 +0000

    initial classroom class + teacher class mod, KISS

---
+++ week5/kiss/classroom.py
@@ -0,0 +1,25 @@
+from teacher import Teacher
+from student import Student
+
+class Classroom:
+
+    def __init__(self, teacher=None):
+        self.__teacher = teacher
+        self.__students = []
+
+    def add_student(self, student):
+        self.__students.append(student)
+
+    def remove_student(self, student):
+        self.__students.remove(student)
+
+    def assign_teacher(self, teacher):
+        self.__teacher = teacher
+
+    def display_information(self):
+        print("Teacher Information:")
+        if self.__teacher:
+            print(self.__teacher.get_information())
+        print("Students Information:")
+        for student in self.__students:
+            print(student.get_information())
--- week5/kiss/teacher.py
+++ week5/kiss/teacher.py
@@ -2,9 +2,14 @@ from person import Person
 
 class Teacher(Person):
 
-    def __init__(self, name, age, employee_id):
+    def __init__(self, name, age, employee_id, address, email, phone):
         super().__init__(name, age)
         self.__employee_id = employee_id
+        self.__address = address
+        self.__email = email
+        self.__phone = phone
 
     def get_information(self):
-        return f'Name: {self.__name}\nAge: {self.__age}\nEmployee ID: {self.__employee_id}'
+        large_string =  f'''Name: {self.__name}\nAge: {self.__age}\nEmployee ID: {self.__employee_id}
+                            \nAddress: {self.__address}\nEmail: {self.__email}\nPhone Number: {self.__phone}'''
+        return large_string

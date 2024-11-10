e69a56efbe7254b00c4b860ee791ad3dd2454b7e
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-10 21:36:57 +0000

    inital game class

--- week5/separation_of_concerns/game_logic.py
+++ week5/separation_of_concerns/game_logic.py
@@ -2,4 +2,25 @@
 class Game:
 
     def __init__(self):
+        self.__current_room = "Entrance"
+        self.__player_health = 100
+        self.__player_inventory = []
+
+    def get_current_room(self):
+        return self.__current_room
+    
+    def get_player_health(self):
+        return self.__player_health
+    
+    def get_player_inventory(self):
+        return self.__player_inventory
+    
+    def move(self):
+        print("You move to another room.")
+        # Code to handle movement logic
+        pass
+
+    def fight(self):
+        print("You encounter a monster!")
+        # Code to handle combat logic
         pass

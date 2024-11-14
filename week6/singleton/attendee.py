aeb0ca8bc00d15c7e7d8a703831c8d15dad033e9
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 10:50:23 +0000

    modified attendee class

--- week6/singleton/attendee.py
+++ week6/singleton/attendee.py
@@ -3,8 +3,15 @@ class Attendee:
 
     initial_id = 1000
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.__name = name
         self.__id = Attendee.initial_id
         Attendee.initial_id += 1
+
+    def get_name(self) ->  str:
+        return self.__name
+    
+    def get_id(self) -> int:
+        return self.__id
+        
 

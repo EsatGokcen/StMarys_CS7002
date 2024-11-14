4c9337b052de02d8b320e513ba07d550ede451f4
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 10:57:22 +0000

    modified event class

--- week6/singleton/event.py
+++ week6/singleton/event.py
@@ -4,3 +4,9 @@ class Event:
     def __init__(self, name, schedule):
         self.__name = name
         self.__schedule = schedule
+
+    def get_name(self) -> str:
+        return self.__name
+    
+    def get_schedule(self) -> str:
+        return self.__schedule

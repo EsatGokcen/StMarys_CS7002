6a8a53e5dcd319f7224ae09dfb2ca1b6870ffe73
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-14 12:51:33 +0000

    fixed error on main func

--- week6/singleton/main.py
+++ week6/singleton/main.py
@@ -4,17 +4,17 @@ from attendee import Attendee
 def main():
     
     res1 = ReservationSystem()
-    res2 = ReservationSystem
+    res2 = ReservationSystem()
 
     attendee1 = Attendee('Esat')
     attendee2 = Attendee('Taha')
 
     print(res1 is res2) #if output True, singleton works
 
-    ReservationSystem.add_attendee(attendee1)
-    ReservationSystem.add_attendee(attendee2)
+    res1.add_attendee(attendee1)
+    res2.add_attendee(attendee2)
     
-    ReservationSystem.retrieve_attendee_info()
+    res1.retrieve_attendee_info()
 
 if __name__ == '__main__':
     main()

c5d70d014494b62b43dcb52983cf54d69ad81535
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-15 21:46:26 +0000

    initial simple factory subdir

---
+++ week6/simple_factory/__init__.py
@@ -0,0 +1,8 @@
+# the simple factory is a creational design pattern
+# that provides an interface for creating objects in a superclass 
+# but allows subclasses to alter the type of objects that will be created. 
+# It is used to encapsulate object creation logic and promote loose coupling 
+# by ensuring that the code that creates objects 
+# does not need to know the specific class of the object being created.
+
+

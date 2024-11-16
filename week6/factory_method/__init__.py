ce9e4d81f9bd3ab0e5225d337a80a06ab2a52033
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-16 23:49:32 +0000

    initial factory method subdir

---
+++ week6/factory_method/__init__.py
@@ -0,0 +1,7 @@
+# Factory Merthod
+# is a creational design pattern that provides an interface 
+# for creating objects but allows subclasses to alter 
+# the type of objects that will be created. 
+# It is used when a class cannot anticipate 
+# the class of objects it must create, 
+# or when a class wants its subclasses to specify the objects it creates.

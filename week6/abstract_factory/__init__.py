020e53357ad4288fdbc16aecdd6cfd6ffb4561cb
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-18 16:17:36 +0000

    initial abstract factory subdir

---
+++ week6/abstract_factory/__init__.py
@@ -0,0 +1,6 @@
+# Abstract Factory Pattern
+# is a creational design pattern that provides an interface 
+# for creating families of related or dependent objects without 
+# specifying their concrete classes. It allows the creation of objects 
+# that are related by a common theme, such as products of the same family, 
+# while ensuring that the created objects are compatible with each other.

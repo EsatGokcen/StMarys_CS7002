e590a87f2bb4e58915ad77904f6f116e91fba8db
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 12:51:02 +0000

    initial concrete human infantry class

---
+++ week6/abstract_factory/human_infantry.py
@@ -0,0 +1,9 @@
+from infantry_unit import InfantryUnit
+
+class HumanInfantry(InfantryUnit):
+
+    def sword_attack(self):
+        return "Human infantry attacks with a great sword!"
+    
+    def shield_block(self):
+        return "Human infantry blocks with a roman shield!"

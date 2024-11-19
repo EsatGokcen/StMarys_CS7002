b0d0e877c4d9ba071bc71ddff9be6a02a7027beb
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 12:52:38 +0000

    initial concrete human cavalary class

---
+++ week6/abstract_factory/human_cavalry.py
@@ -0,0 +1,9 @@
+from cavalary_unit import CavalaryUnit
+
+class HumanCavalary(CavalaryUnit):
+
+    def charge(self):
+        return "Human cavalary charges!"
+    
+    def lance_attack(self):
+        return "Human cavalary carries a lance attack!"

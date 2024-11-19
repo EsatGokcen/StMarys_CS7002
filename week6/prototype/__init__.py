fcf7668e866ba78adacb5da0128ae7eecb8231f3
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 13:16:10 +0000

    initial prototype pattern subdir

---
+++ week6/prototype/__init__.py
@@ -0,0 +1,5 @@
+# Prototype Pattern 
+# involves defining a prototype interface or abstract class with a cloning method, 
+# and concrete prototype classes that implement this interface 
+# or extend this abstract class. Clients interact with prototype objects 
+# by requesting new instances through the cloning method.

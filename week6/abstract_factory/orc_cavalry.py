1862bc35d4399394ba99859d91c56fc67c4cdce4
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 12:55:58 +0000

    initial concrete orc cavalary class

---
+++ week6/abstract_factory/orc_cavalry.py
@@ -0,0 +1,9 @@
+from cavalary_unit import CavalaryUnit
+
+class OrcCavalary(CavalaryUnit):
+
+    def charge(self):
+        return "Orc cavalary units charge with warewolves!"
+    
+    def lance_attack(self):
+        return "Orc cavalary units attack with lance!"

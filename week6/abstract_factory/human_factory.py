d2ef88c52c97346a75ea82c992a3f9a7d3608ff5
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 12:58:06 +0000

    modified human factory class to include human unit classes

--- week6/abstract_factory/human_factory.py
+++ week6/abstract_factory/human_factory.py
@@ -1,9 +1,11 @@
 from unit_factory import UnitFactory
+from human_cavalry import HumanCavalary
+from human_infantry import HumanInfantry
 
 class HumanUnitFactory(UnitFactory):
 
     def create_infantry(self):
-        return "Created human infantry unit."
+        return HumanInfantry()
     
     def create_cavalry(self):
-        return "Created human cavalry unit."
+        return HumanCavalary()

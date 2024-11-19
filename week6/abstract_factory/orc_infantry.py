dd72a0cc75b8c1801cb559147b4377546e637d6f
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-19 12:54:37 +0000

    initial concrete orc infantry class

---
+++ week6/abstract_factory/orc_infantry.py
@@ -0,0 +1,9 @@
+from infantry_unit import InfantryUnit
+
+class OrcInfantry(InfantryUnit):
+
+    def sword_attack(self):
+        return "Orc infantry attacks with a cleaver!"
+    
+    def shield_block(self):
+        return "Orc infantry defends with a round shield!"

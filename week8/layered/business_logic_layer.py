a751fec122d50a9cc430e9d77c56ef296384c717
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 09:56:34 +0000

    add customer methods

--- week8/layered/business_logic_layer.py
+++ week8/layered/business_logic_layer.py
@@ -2,3 +2,11 @@ class BusinessLogicLayer:
 
     def __init__(self, data_layer):
         self.__data_layer = data_layer
+
+    def add_customer(self, customer):
+        self.__data_layer.create_customer(customer)
+    
+    def add_customers(self, customers):
+        for customer in customers:
+            self.__data_layer.create_customer(customer)
+        

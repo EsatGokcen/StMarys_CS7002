4d8d4555ddd9b8c81a7380bfbcca5f0f45e68e36
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 12:18:14 +0000

    linked MVC classes and added controller methods

--- week8/MVC/controller/stock_controller.py
+++ week8/MVC/controller/stock_controller.py
@@ -1,5 +1,17 @@
+from model.stock_model import StockModel
+from view.stock_view import StockView
 
 class StockController:
 
     def __init__(self):
-        pass
+        self.__model = StockModel(name="Nvidia", price="100")
+        self.__view = StockView(self.__model)
+
+    def set_stock(self, name, price):
+        self.__model.set_name(name)
+        self.__model.set_price(price)
+
+    def display_stock(self):
+        self.__view.display_stock()
+
+    

95f0ec6b40d5ae7a1bb5b0944de06abf4c82c134
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:41:59 +0000

    initial decorator subdir

---
+++ week7/decorator/__init__.py
@@ -0,0 +1,5 @@
+# The Decorator Pattern 
+# involves creating a set of decorator classes that are used 
+# to wrap concrete components. Each decorator class implements 
+# the same interface as the component it wraps, allowing decorators to be 
+# stacked or chained together to add multiple layers of behaviour.

050193c8362fbda4b347c3c8301799ad65cdc024
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 12:31:45 +0000

    tested main func, working

--- week8/MVC/main.py
+++ week8/MVC/main.py
@@ -3,6 +3,7 @@ from controller.stock_controller import StockController
 def main():
     stock_controller = StockController()
     stock_controller.set_stock("Nvidia" , 150)
+    stock_controller.display_stock()
 
 if __name__ == '__main__':
     main()

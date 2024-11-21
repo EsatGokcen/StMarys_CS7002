d4a3d25e144fe4332f5319362209b6e8f3a1cd09
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:44:58 +0000

    initial open weather app api file

---
+++ week7/adapter/open_weather_app_api.py
@@ -0,0 +1,13 @@
+# OpenWeatherMap API Client
+class OpenWeatherMapAPI:
+    def get_temperature(self):
+        # Simulate fetching temperature data from OpenWeatherMap API
+        return {'temp': 25}
+
+    def get_humidity(self):
+        # Simulate fetching humidity data from OpenWeatherMap API
+        return {'humidity': 70}
+
+    def get_wind_speed(self):
+        # Simulate fetching wind speed data from OpenWeatherMap API
+        return {'wind_speed': 10}

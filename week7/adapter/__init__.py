95809377c9f9dd77aaec20e42c1f53728e026aee
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:40:09 +0000

    initial adapter subdir

---
+++ week7/adapter/__init__.py
@@ -0,0 +1,6 @@
+# Adapter Pattern 
+# This is a structural design pattern that allows 
+# incompatible interfaces to work together. 
+# It acts as a bridge between two incompatible interfaces by 
+# providing a wrapper or intermediary that translates the interface of 
+# one class into another interface that a client expects.

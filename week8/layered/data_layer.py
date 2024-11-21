5a353ce2e78cf8febb9472bba79e9e37dee45983
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 10:14:34 +0000

    add unique id to customer in data layer

--- week8/layered/data_layer.py
+++ week8/layered/data_layer.py
@@ -1,10 +1,16 @@
 class DataLayer:
 
+    next_id = 1
+
     def __init__(self):
         self.__customers = []
 
     def create_customer(self, customer):
+        id = DataLayer.next_id
+        customer = [id, *customer] # * unpacks the customer variable
         self.__customers.append(customer)
+        DataLayer.next_id += 1
+        return customer
 
     def retrieve_customer(self, customer_id):
         for customer in self.__customers:

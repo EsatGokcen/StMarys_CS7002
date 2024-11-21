aef2fe453ee70413f2d127c23f27b295c4c6477c
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 11:03:39 +0000

    display customers method in presentation layer

--- week8/layered/presentation_layer.py
+++ week8/layered/presentation_layer.py
@@ -2,3 +2,9 @@ class PresentationLayer:
 
     def __init__(self, business_logic_layer):
         self.__business_logic_layer = business_logic_layer
+
+    def display_all_customers(self):
+        customers = self.__business_logic_layer.get_customers()
+        print("id | first name | last name | date of birth")
+        for customer in customers:
+            print(f"{customer[0]:<4} | {customer[1]:<10} | {customer[2]:<10} | {customer[3]}")

1704342be52f185b6b7577949d3b0e8a662c98d2
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 12:05:03 +0000

    display_stock method in stockview class

--- week8/MVC/view/stock_view.py
+++ week8/MVC/view/stock_view.py
@@ -1,9 +1,10 @@
 
 class StockView:
 
-    def __init__(self):
-        pass
+    def __init__(self, stock_model):
+        self.__stock_model = stock_model
 
     def display_stock(self):
-        pass
+        print(f"Stock Name: {self.__stock_model.get_name()}")
+        print(f"Stock Price: {self.__stock_model.get_price()}")
         

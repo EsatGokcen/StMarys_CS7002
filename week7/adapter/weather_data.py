58735461d5272cf0a5d3ec5212f3d690e37da9f8
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:47:33 +0000

    initial weather data interface class

---
+++ week7/adapter/weather_data.py
@@ -0,0 +1,10 @@
+# Unified Weather Data Interface
+class WeatherData:
+    def get_temperature(self):
+        pass
+
+    def get_humidity(self):
+        pass
+
+    def get_wind_speed(self):
+        pass

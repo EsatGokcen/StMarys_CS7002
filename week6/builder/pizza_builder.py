aa5dda94e7d4ed741b7acf9398a75978cff99912
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:04:28 +0000

    initial pizza builder interface

---
+++ week6/builder/pizza_builder.py
@@ -0,0 +1,15 @@
+# interface 
+
+class PizzaBuilder:
+
+    def add_topping(self, topping):
+        pass
+
+    def select_crust(self, crust_type):
+        pass
+
+    def choose_size(self, size):
+        pass
+
+    def get_pizza(self):
+        pass

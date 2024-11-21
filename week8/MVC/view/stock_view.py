68f232d155e5103c0fa038a648ced5cb08140300
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 12:19:27 +0000

    changed view class, display method name to refresh

--- week8/MVC/view/stock_view.py
+++ week8/MVC/view/stock_view.py
@@ -4,7 +4,7 @@ class StockView:
     def __init__(self, stock_model):
         self.__stock_model = stock_model
 
-    def display_stock(self):
+    def refresh(self):
         print(f"Stock Name: {self.__stock_model.get_name()}")
         print(f"Stock Price: {self.__stock_model.get_price()}")
         

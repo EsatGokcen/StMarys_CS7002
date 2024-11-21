eec0b185add6d847ad590154318f5a430b5b7bfd
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 16:45:37 +0000

    initial weather stack api file

---
+++ week7/adapter/weather_stack_api.py
@@ -0,0 +1,5 @@
+# WeatherStack API Client
+class WeatherStackAPI:
+    def get_current(self):
+        # Simulate fetching current weather data from WeatherStack API
+        return {'temperature': 24, 'humidity': 68, 'wind_speed': 12}

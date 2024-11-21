f2509c7009fda2a04691bab65662bf6cb11416b9
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-21 15:59:08 +0000

    initial builder class

---
+++ week6/builder/__init__.py
@@ -0,0 +1,4 @@
+# The Builder Pattern 
+# involves defining a builder interface or abstract class with methods 
+# for building different parts of the object and concrete builder classes 
+# that implement this interface to construct the object step by step.

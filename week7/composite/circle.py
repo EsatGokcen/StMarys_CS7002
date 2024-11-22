172d24d282f7668cafc367f51377ccb469c5dabe
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-22 18:28:09 +0000

    initial circle leaf class

---
+++ week7/composite/circle.py
@@ -0,0 +1,15 @@
+# Leaf classes
+from component_interface import Shape
+
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self):
+        return f"Circle (x={self.x}, y={self.y}, radius={self.radius})"
+
+    def transform(self, transformation):
+        # Apply transformation to circle (e.g., rotation, scaling)
+        pass

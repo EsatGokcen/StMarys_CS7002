6c9389bc7328d05037707a4cf6af7f9d66dcbaef
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-25 17:47:34 +0000

    initial observer subdir

---
+++ week7/observer/__init__.py
@@ -0,0 +1,9 @@
+# the Observer Pattern. 
+# This is is a behavioural design pattern that defines 
+# a one-to-many dependency relationship between objects, 
+# so that when one object (the subject or observable) changes state, 
+# all its dependents (observers) are notified and updated automatically. 
+# This pattern is used to establish communication between objects in a 
+# loosely coupled manner, where the subject and observers remain 
+# independent of each other.
+

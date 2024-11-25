9e270b15f4d256b511b93ba6fe1955ec51e6ec9d
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-25 17:41:20 +0000

    initial subsystem security camera

---
+++ week7/facade/security_camera.py
@@ -0,0 +1,7 @@
+# Subsystem: Security Camera
+class SecurityCamera:
+    def arm(self):
+        print("Arming security cameras")
+
+    def disarm(self):
+        print("Disarming security cameras")

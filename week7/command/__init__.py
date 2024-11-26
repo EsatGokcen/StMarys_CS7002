4e260f36fbf32d58bc9f4c45c8aa815d6475907b
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-26 15:34:32 +0000

    initial command subdir

---
+++ week7/command/__init__.py
@@ -0,0 +1,5 @@
+# The Command Pattern 
+# allows the creation of command objects that encapsulate a 
+# specific action along with its parameters, 
+# providing a way to parameterise clients with 
+# queues, requests, and operations.

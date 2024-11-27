5c05613f4c4d10a09effa08c9e9f8d605ca17234
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-27 16:37:01 +0000

    initial tax view class

---
+++ week8/mvc_with_observer/tax_view.py
@@ -0,0 +1,7 @@
+# View
+
+class TaxView:
+    
+    def display_tax_summary(self, total_income, tax_liability):
+        print("Total Income:", total_income)
+        print("Tax Liability:", tax_liability)

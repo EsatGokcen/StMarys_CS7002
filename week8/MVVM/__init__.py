f471a00709468fe5b2b0b1fe41485896dd83a083
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-27 23:54:02 +0000

    initial MVVM subdir

---
+++ week8/MVVM/__init__.py
@@ -0,0 +1,6 @@
+# MVVM Pattern
+# In MVC, the Controller mediates between Model and View, 
+# while in MVVM, the ViewModel abstracts View from Model, 
+# promoting better separation of concerns. MVVM leverages data binding for 
+# automatic updates between View and ViewModel, enhancing testability and 
+# maintainability.

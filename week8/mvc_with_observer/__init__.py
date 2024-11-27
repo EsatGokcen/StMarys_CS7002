a4bbe04e6e7b3a82dce80442ef151edaa26cd828
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-27 16:25:09 +0000

    initial mvc with observer subdir

---
+++ week8/mvc_with_observer/__init__.py
@@ -0,0 +1,10 @@
+# Model-View-Controller (MVC) with Observer 
+# the View autonomously reacts to changes in the Model without 
+# direct intervention from the Controller. This paradigm shift is 
+# facilitated by modern frameworks and libraries that support reactive 
+# programming and data binding, enabling seamless synchronisation between 
+# the UI and underlying data. This approach enhances application responsiveness,
+# facilitates real-time updates, and simplifies testing and maintenance by 
+# promoting better separation of concerns. It finds utility across various 
+# domains, including front-end web development, desktop, and mobile applications, 
+# especially in contexts requiring dynamic and highly responsive user interfaces.

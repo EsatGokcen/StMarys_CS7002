0c68200ae618a5ae46ea0ad02bba99f4b86961a9
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-28 00:15:29 +0000

    initial event driven architecture subdir

---
+++ week8/EDA/__init__.py
@@ -0,0 +1,6 @@
+# Event-Driven Architecture (EDA) 
+# is a software design pattern that revolves around the concept of events. 
+# In this architecture, the flow of the system is determined by events that 
+# occur within it. An event can be any significant occurrence or change 
+# in state within the system, such as a user action, a sensor reading, 
+# a message arrival, or a database update.

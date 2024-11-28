e7fb5c6c62c227af5eda6d194ada92fdf72714ea
Esat Gokcen <esatgokcen173@gmail.com>
2024-11-28 11:53:25 +0000

    created set up method for unittest

--- week9/unittest/unittest_calculator.py
+++ week9/unittest/unittest_calculator.py
@@ -3,8 +3,10 @@ from simple_calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
+    def set_up(self):
+        self.calculator = Calculator()
+
     def test_add(self):
-        calculator = Calculator()
-        actual = calculator.add(5,3)
+        actual = self.calculator.add(5,3)
         expectation = 8
         self.assertEqual(actual, expectation)
